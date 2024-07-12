6093e2558ed281ce3e803f112a86cdb09d1d5811
BiBa <andreasjehle89@web.de>
2024-07-12 11:15:21 +0200

    fixed better usleep

--- src/time.c
+++ src/time.c
@@ -16,6 +16,20 @@ long	get_time_in_millis(void)
 	return((time.tv_sec * 1000) + (time.tv_usec / 1000)); 
 }
 
+long	get_time_in_micros(void)
+{
+	struct	timeval time;
+
+	gettimeofday(&time, NULL);
+	//time in sec * 1000000	= time in us
+	//time in usec			= time in us
+	//						+
+	//----------------------------------
+	//	total time in us	= 
+	return((time.tv_sec * 1000000) + time.tv_usec); 
+}
+
+
 void	better_usleep(long time_in_micros)
 {
 	long	start;	
@@ -31,29 +45,6 @@ void	better_usleep(long time_in_micros)
 		current = get_time_in_micros();
 		time_gone = current - start;
 		if(time_gone > time_in_micros)
-			break;
-		usleep(1);
-	}
-	return;
-}
-
-
-void	better_usleep(t_table *table, long time_in_micros)
-{
-	struct	timeval current;
-	struct	timeval start_usleep;
-	long	time_gone;	
-
-	if(dining_finished(table))	
-		return;
-	gettimeofday(&start_usleep, NULL);
-	if(table->end_of_dinner)
-		return;
-	while(!dining_finished(table))
-	{
-		gettimeofday(&current, NULL);
-		time_gone = (current.tv_sec - start_usleep.tv_sec) * 1000000 + (current.tv_usec - start_usleep.tv_usec);
-		if(time_gone >= time_in_micros)
 			break;
 		usleep(1);
 	}
